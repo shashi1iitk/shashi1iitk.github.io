22cf078d66a08fa9418f594c9322c90fba33cf8e
Shashi Ranjan <shashi1iitk@gmail.com>
2017-07-09 09:44:51 +0530

    privacy

--- login/databaseC.php
+++ login/databaseC.php
@@ -1,5 +1,5 @@
 <?php
-if(mysql_connect('localhost','id1609175_shashi1iitkgp','Suraj@9631')){
+if(mysql_connect('localhost','************','**********')){
 if(mysql_select_db('id1609175_database1212'))
 	;
 else
